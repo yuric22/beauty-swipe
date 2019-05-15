7dce6bc4dba7c51c10c517bf49b1df51eb514c2c
Yuri Cardenas <yuricardenas@gmail.com>
2019-05-14 21:42:47 -0300

    Implements like/dislike count + products refetch

--- src/BeautySwipe.js
+++ src/BeautySwipe.js
@@ -5,6 +5,8 @@ import { API } from './api.js';
 
 import btnDislike from './btn-dislike.svg';
 import btnLike from './btn-like.svg';
+import counterDislike from './counter-dislike.svg';
+import counterLike from './counter-like.svg';
 
 import './BeautySwipe.css';
 
@@ -21,6 +23,8 @@ class BeautySwipe extends React.Component {
             error: false,
             brands: [],
             page: 0,
+            likes: 0,
+            dislikes: 0,
         }
     }
 
@@ -66,36 +70,57 @@ class BeautySwipe extends React.Component {
     handleLikeClick = () => {
         // weightedBrands.addWeight(this.state.currentCard, 1);
         this.getNextCard();
+        this.setState({
+            likes: this.state.likes + 1,
+        });
     }
 
     handleDislikeClick = () => {
         // weightedBrands.addWeight(this.state.currentCard, -1);
         this.getNextCard();
+        this.setState({
+            dislikes: this.state.dislikes + 1,
+        });
     }
 
     getNextCard = () => {
         // const newCard = weightedBrands.peek();
         // console.log("PEEK", newCard)
-        // this.setState({
-        //     currentCard: newCard,
-        // });
+        const updatedCards = [].concat(this.state.cards);
+        updatedCards.shift();
+        this.setState({
+            cards: updatedCards,
+            currentCard: this.state.cards[1],
+        });
+
+        if (updatedCards.length === 1){
+            this.setState({
+                page: this.state.page + 1
+            }, this.getProducts)
+        }
     }
 
     render() {
         return (
             <div className="beauty-swipe-wrapper">
+                <div className="interaction-count">
+                    <div><img className="counter" src={counterDislike} alt="dislike counter" /> {this.state.dislikes}</div>
+                    <div>{this.state.likes} <img className="counter" src={counterLike} alt="like counter" /></div>
+                </div>
                 {this.state.loading ?
                     <Spinner />
                 :
                     <div>
                         <div className="card">
-                            <img src={this.state.currentCard.imageUrl} alt="Product" className="card-img" />
+                            <div className="body">
+                                <img src={this.state.currentCard.imageUrl} alt="Product" className="card-img" />
+                            </div>
                             <div className="footer">
                                 <h1>{this.state.currentCard.name}</h1>
                                 <h2>{this.state.currentCard.brand}</h2>
                             </div>
                         </div>
-                        <div className="buttons">
+                        <div className="interaction-buttons">
                             <img className="btn-dislike" src={btnDislike} onClick={this.handleDislikeClick} alt="dislike" />
                             <img className="btn-like" src={btnLike} onClick={this.handleLikeClick} alt="like" />
                         </div>
